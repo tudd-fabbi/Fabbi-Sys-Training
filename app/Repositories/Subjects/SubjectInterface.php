cc05914758a4911332e7af24ed2e2bebae0490b8
HuyDuc05021998 <nguyenduc05021998@gmail.com>
2020-11-12 16:03:33 +0700

    update/BE/Api/Add-Update-Delete/Subject

--- app/Repositories/Subjects/SubjectInterface.php
+++ app/Repositories/Subjects/SubjectInterface.php
@@ -8,11 +8,9 @@ interface SubjectInterface extends BaseRepositoryInterface
 {
     public function getListSubject($request);
     public function deleteSubject($id);
-    public function createSubject($request);
-    public function updateSubject($request);
+    public function createSubject(array $data);
+    public function updateSubject(array $data, $id);
     public function getSubjectById($id);
-    public function countTaskById($id);
-    public function countCourseById($id);
-    public function countUserById($id);
-    public function listCourse($id);
+    public function countTaskCourseSubjectById($id);
+    public function ListCourseBySubjetID($id);
 }
