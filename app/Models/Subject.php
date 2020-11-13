2cf7268d4b09e56632ba3407cb7b2b2039613c39
Anhkieu123 <kieutuananh2121999@gmail.com>
2020-11-13 08:25:24 +0700

    fix create task, show task

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subject extends Model
 {
-    function userSubject(){
-        return $this->belongsToMany(User::class, 'user_subject', 'subject_id', 'user_id');
+    function users(){
+        return $this->belongsToMany(User::class, 'user_subject');
     }
-    function taskSubject(){
-        return $this->belongsToMany(Task::class, 'subject_task', 'subject_id', 'task_id');
+    function tasks(){
+        return $this->belongsToMany(Task::class, 'subject_task');
     }
 }
---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'task' => [
+        'status' => 1
+    ]
+];
