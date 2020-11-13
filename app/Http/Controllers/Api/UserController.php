419384fbc8b788190f2049c118e15418b7bea243
PhanVinhKhanh <Khanh161297@gmail.com>
2020-11-13 16:11:51 +0700

    Fix review Write API AddUser

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Repositories\Users\UserRepositoryInterface;
+use App\Http\Controllers\Api\ApiBaseController;
+
+class UserController extends ApiBaseController
+{
+    protected $repository;
+
+    public function __construct(UserRepositoryInterface $repository){
+        $this->repository = $repository;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $inputData = $request->only(
+            'name',
+            'birthday',
+            'phone',
+            'address',
+            'email',
+            'password',
+            'course',
+            'img_path'
+        );
+
+        $data = $this->repository->addUser($inputData);
+        if ($data['success']) {
+            return $this->sendError(500, "Error", "Failed");
+        }
+
+        return $this->sendSuccess('Add User Success');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Repositories/User/UserRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories\Users;
+
+use App\Repositories\BaseRepositoryInterface;
+
+interface UserRepositoryInterface extends BaseRepositoryInterface
+{
+    public function addUser(array $data);
+}
