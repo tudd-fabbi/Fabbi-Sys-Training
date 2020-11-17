4ec7bd47c4a3e8959cd71fc1076c40d3c7bb20ba
PhanVinhKhanh <Khanh161297@gmail.com>
2020-11-17 14:11:57 +0700

    Update API

--- app/Repositories/User/UserRepositoryInterface.php
+++ app/Repositories/User/UserRepositoryInterface.php
@@ -8,5 +8,8 @@ interface UserRepositoryInterface extends BaseRepositoryInterface
 {
     public function addUser(array $data);
     public function getListUser(array $data);
+    public function countSubjectById($id);
+    public function countTaskById($id);
+    public function getUserNameById($id);
 }
 
